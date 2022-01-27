cfce36f134302e00c6ae65e4e7e3c3a96f43e68f
Ayush Panwar <46302849+payyup@users.noreply.github.com>
2022-01-28 05:17:10 +0530

    Delete axios.config.js

--- src/actions/axios.config.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:5000/api/';
-axios.defaults.withCredentials = true;
-
-const client = axios.create({});
-
-export default client;
